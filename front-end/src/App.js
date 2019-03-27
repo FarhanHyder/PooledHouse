554fb285c579878b6c69e3191478e6ab094069c3
Farhan <fmshyder@gmail.com>
2019-03-27 00:11:40 -0400

    homepage init

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// react-bootstrap
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Navbar className="bg-dark justify-content-between" variant="dark">
+        <Navbar.Brand href="#home">Pooled House</Navbar.Brand>
+
+          <Button type="" variant="">Change View</Button>
+
+          <Form inline>
+            <FormControl type="text" placeholder="ex: upper manhattan" className=" mr-sm-2" />
+            <Button type="submit" variant="outline-info">Search</Button>
+          </Form>
+
+          <Button type="" variant="">Create New Account</Button>
+          <Button type="" variant="">Log In</Button>
+          
+      </Navbar>
 
       </div>
     );
