23107be6ed18d0f4895ae2f9b68a436c2a862dad
Bashir Ahamed <ahamedb1991@gmail.com>
2019-04-03 22:12:06 -0400

    simple form input for job title

--- front-end/src/Components/GetTrip.js
+++ front-end/src/Components/GetTrip.js
@@ -3,7 +3,28 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 const getTipsData = (props) => {
+    <Form className="text-left newTips" onSubmit={props.data}>
+        <Form.Group controlId="tipData">
 
+            <div className="inputBlock">
+                <Form.Label>Job Title</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter Your Job Title"
+                    value={}
+                    onChange={/* function to update state*/}/>
+            </div>
+
+            <div className="inputBlock">
+            
+            </div>
+
+            <div className="inputBlock">
+
+            </div>
+
+        </Form.Group>
+    </Form>
 }
 
 export default getTipsData;
