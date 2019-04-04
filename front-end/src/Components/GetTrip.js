60558315707bfd77da747fb72bb49be5fcefeef7
Bashir Ahamed <ahamedb1991@gmail.com>
2019-04-03 22:05:43 -0400

    initial input function

--- front-end/src/Components/GetTrip.js
+++ front-end/src/Components/GetTrip.js
@@ -1,2 +1,9 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
+const getTipsData = (props) => {
+
+}
+
+export default getTipsData;
