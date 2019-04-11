3ce60713e951251e860c713646f493e0f8da4951
dgarsir <welch.carlton@gmail.com>
2019-04-11 10:26:33 -0400

    tipupdate formatting

--- front-end/src/TipInfo/TipInfo.js
+++ front-end/src/TipInfo/TipInfo.js
@@ -1,41 +1,40 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
+import {Col, Row, Container, Form, Button} from 'react-bootstrap';
+//import Form from 'react-bootstrap/Form';
+//import Button from 'react-bootstrap/Button';
 
 const TipInfo = (props) => {
     return (
-    <Container>
+    <Container fluid='true'>
         <Form className="text-left newTips" onSubmit={()=>{console.log(props.name)}}>
+            <Row>
+                <Col>
             <Form.Group controlId="shiftDate">
                     <Form.Label>Shift Date</Form.Label>
                     <Form.Control
                         type='date'
                     />
             </Form.Group> 
+            </Col>
+            <Col>
             <Form.Group controlId="shiftAP">
+                <Form.Label>Shift Time</Form.Label>
+                { /*this div just helps with formatting */}
+                <div>
                     <Form.Check inline label='AM' type='radio' name='rad' id='radio-AM' />
                     <Form.Check inline label='PM' type='radio' name='rad' id='radio-PM' />
+                </div>
             </Form.Group>
+            </Col>
+            <Col>
             <Form.Group>
                 <Form.Label>Shift Length (hours)</Form.Label>
                 <Form.Control
                     type='number'
                 />
             </Form.Group>
-            <Form.Group>
-                <Form.Label>Tip Takehome</Form.Label>
-                <Form.Control
-                    type='number'
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Shift Location</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='i.e. 123 E 29th St, New York, New York, 99999'
-                />
-            </Form.Group>
+            </Col>
+            <Col>
             <Form.Group>
                     <Form.Label>Position</Form.Label>
                     <Form.Control as='select'>
@@ -46,10 +45,28 @@ const TipInfo = (props) => {
                         <option>Other</option>
                     </Form.Control>
             </Form.Group>
+            </Col>
+            </Row>
+            <Row>
+                <Col>
             <Form.Group>
-
+                <Form.Label>Tip Takehome</Form.Label>
+                <Form.Control
+                    type='number'
+                />
+            </Form.Group>
+                </Col>
+            <Col>
+            <Form.Group>
+                <Form.Label>Shift Location</Form.Label>
+                <Form.Control
+                    type='text'
+                    placeholder='i.e. 123 E 29th St, New York, New York, 99999'
+                />
             </Form.Group>
             <Button variant="primary" type = "submit">Submit</Button>
+            </Col>
+            </Row>
         </Form>
     </Container>
     );
