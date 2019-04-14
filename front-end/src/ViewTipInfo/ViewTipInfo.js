9f0238aa13d98c2dbb62308ab038165046ce680f
Bashir Ahamed <ahamedb1991@gmail.com>
2019-04-14 18:25:16 -0400

    add filter

--- front-end/src/ViewTipInfo/ViewTipInfo.js
+++ front-end/src/ViewTipInfo/ViewTipInfo.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 const ViewTipInfo = (props) => {
-    let tipInfo = props.tipsInfo.map( (tips, index) => {
-        if(tips.position === props.position) {
-            return (
-                <div className="tipInfo">
-                    <div>Business Name : {tips.business_name}</div>
-                    <div>Address : {tips.business_address}</div>
-                    <div> Tips : {tips.takehome}</div>
-                    <div>Hours Worked : {tips.shift_length}</div>
-                    <div>Work Shift : {tips.shift_time}</div>
-                    <div>Date : {tips.shift_date}</div>
-                </div>
-            );
-        }          
+    let filterdTipInfo = props.tipInfo.filter(tips => tips.position === props.position);
+
+    let tipInfo = filterdTipInfo.map( (tips, index) => {
+        return (
+            <div className="tipInfo">
+                <div>Business Name : {tips.business_name}</div>
+                <div>Address : {tips.business_address}</div>
+                <div> Tips : {tips.takehome}</div>
+                <div>Hours Worked : {tips.shift_length}</div>
+                <div>Work Shift : {tips.shift_time}</div>
+                <div>Date : {tips.shift_date}</div>
+            </div>
+        );        
     });
 
     return tipInfo;
