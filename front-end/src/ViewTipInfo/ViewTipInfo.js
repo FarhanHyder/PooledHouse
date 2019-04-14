21d6d64e5f66eea94ac35f0f1bac8a08592146f1
Bashir Ahamed <ahamedb1991@gmail.com>
2019-04-14 16:32:22 -0400

    view name hour and tips

--- front-end/src/ViewTipInfo/ViewTipInfo.js
+++ front-end/src/ViewTipInfo/ViewTipInfo.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const ViewTipInfo = (props) => {
-    let tipInfo = props.tipsInfo.map(
-        
-    );
+    let tipInfo = props.tipsInfo.map( (tips, index) => {
+        if(tips.position === props.position) {
+            return (
+                <div className="tipInfo">
+                    <div>{tips.business_name}, {tips.business_address}</div>
+                    <div>{tips.takehome}, {tips.shift_length}</div>
+                </div>
+            );
+        }          
+    });
 
     return tipInfo;
 }
