77c47e68116137e056ef48c1839e997da644fb12
Bashir Ahamed <ahamedb1991@gmail.com>
2019-04-17 15:24:21 -0400

    unfiltered temp view

--- front-end/src/Components/ViewTipInfo/ViewTipInfo.js
+++ front-end/src/Components/ViewTipInfo/ViewTipInfo.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 
+
 const ViewTipInfo = (props) => {
-    let position = 'Bartender';
-    // <Form.Group>
+    // let position = 'Bartender';
+    // <Form.Group onSubmit = {position = this.event.value}>
     //     <Form.Label>Position</Form.Label>
     //     <Form.Control as='select'>
     //         <option>Bartender</option>
@@ -14,13 +15,15 @@ const ViewTipInfo = (props) => {
     //         <option>Other</option>
     //     </Form.Control>
     // </Form.Group>
-    let filterdTipInfo = props.tipInfo.filter(tips => tips.position === position);
+   
+    // let filterdTipInfo = props.tipInfo.filter(tips => tips.position === position);
 
-    let tipInfo = filterdTipInfo.map( (tips, index) => {
+    let tipInfo = props.tipInfo.map( (tips, index) => {
         return (
             <div className="tipInfo">
                 <div>Business Name : {tips.business_name}</div>
                 <div>Address : {tips.business_address}</div>
+                <div>Position: {tips.position}</div>
                 <div>Tips : {tips.takehome}</div>
                 <div>Hours Worked : {tips.shift_length}</div>
                 <div>Work Shift : {tips.shift_time}</div>
