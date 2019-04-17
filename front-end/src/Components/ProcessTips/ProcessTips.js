ead8fdcbea9453423805d72b95e6376ca82ee71b
Bashir Ahamed <ahamedb1991@gmail.com>
2019-04-17 15:30:44 -0400

    initial component for processing tips data

---
+++ front-end/src/Components/ProcessTips/ProcessTips.js
@@ -0,0 +1,6 @@
+// data will be process here
+// filter all the data based on business name
+// take the average
+// for each position
+// tips total = takeHome * hour
+// averge total by total employee count
