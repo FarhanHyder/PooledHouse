d422466c5dc4d16d48caad8a6c0ac0200b45c0ec
Bashir Ahamed <ahamedb1991@gmail.com>
2019-04-17 19:36:31 -0400

    fixed missing position in viewTipInfo

--- front-end/src/Components/ViewTipInfo/ViewTipInfo.js
+++ front-end/src/Components/ViewTipInfo/ViewTipInfo.js
@@ -26,7 +26,7 @@ const ViewTipInfo = (props) => {
             <div className="tipInfo">
                 <div>Business Name : {tips.business_name}</div>
                 <div>Address : {tips.business_address}</div>
-                <div>Position: {tips.position}</div>
+                <div>Position: {tips.shift_position}</div>
                 <div>Tips : {tips.takehome}</div>
                 <div>Hours Worked : {tips.shift_length}</div>
                 <div>Work Shift : {tips.shift_time}</div>
