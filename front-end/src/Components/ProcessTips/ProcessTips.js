5d354cb740f037c7bac3f769d20d1fe1d2337f38
Bashir Ahamed <ahamedb1991@gmail.com>
2019-04-17 22:25:48 -0400

    average hourly tips processing by business

--- front-end/src/Components/ProcessTips/ProcessTips.js
+++ front-end/src/Components/ProcessTips/ProcessTips.js
@@ -5,11 +5,32 @@
 // tips total = takeHome * hour
 // average total by total employee count
 
+// shift_length
+// shift_time
+// shift_date
+// shift_position
+// takehome
+// business_name
+// business_address
 
 import React from 'react';
 
 const averageTipsByBusiness = (props) => {
+    const business = [];
+    props.tipInfo.forEach(tips => {
+        let businessName = tips.business_name;
+        if (! business.hasOwnProperty(businessName)) {
+            business.businessName = {tipsPerHour: (tips.takehome / tips.shift_length), totalHour: tips.shift_length};
+        }
+        else {
+            let hours = (business.businessName.totalHour + tips.shift_length);
+            business.businessName.tipsPerHour = business.businessName.tipsPerHour * business.businessName.totalHour / hours + tips.takehome / hours;
+            business.businessName.totalHour = hour;
+        }
+       
+    });
 
+    return business;
 }
 
 const averageTipsByPosition = (props) => {
@@ -21,5 +42,11 @@ const averageTipsByZipCode = (props) => {
 }
 
 const averageTipsByPositionAndShift = (props) => {
+
+}
+
+const ViewTipsAverage = (props) => {
+    // user selects the view option
+    // function to process based on data called from above
 
 }
