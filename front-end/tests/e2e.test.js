0a09fed442385f7bdc18f2c9177656e6968a4053
Shofiqur Rahman <shofi384@gmail.com>
2019-04-18 22:51:51 -0400

    fix browser.newPage() error

--- front-end/tests/e2e.test.js
+++ front-end/tests/e2e.test.js
@@ -1,1 +1,14 @@
 // All end-to-end tests will reside here
+
+const puppeteer = require ('puppeteer');
+
+test("Should return logged-in user's username", async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size=840,630']
+  });
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000/');
+  await page.click('input');
+});
