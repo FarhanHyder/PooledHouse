b21ba49b2ddbbc6b4ef697dabda1d5f5c940bb03
Shofiqur Rahman <shofi384@gmail.com>
2019-04-19 21:02:24 -0400

    add complete e2e log-in test

--- front-end/tests/e2e.test.js
+++ front-end/tests/e2e.test.js
@@ -1,5 +1,8 @@
 // All end-to-end tests will reside here
 
+// Set timeout so that all tests have enough time to finish
+//jest.setTimeout(10000);
+
 const puppeteer = require ('puppeteer');
 
 test("Should return logged-in user's username", async () => {
@@ -8,7 +11,19 @@ test("Should return logged-in user's username", async () => {
     slowMo: 80,
     args: ['--window-size=840,630']
   });
+
   const page = await browser.newPage();
   await page.goto('http://localhost:3000/');
-  await page.click('input');
-});
+
+  await page.click('input[name=username]');
+  await page.type('input[name=username]', 'shofi');
+  await page.click('input[name=password]');
+  await page.type('input[name=password]', '9293767625');
+  await page.click('button[type="submit"]');
+
+  await page.waitFor(1000);
+
+  const userName = await page.$eval('div.Nav__navItem___VnHJi', el => el.textContent);
+  expect(userName).toBe('Hello shofi');
+}, 10000);
+
