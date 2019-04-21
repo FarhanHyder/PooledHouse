4676946e64a5124e4ef7045d795ebf30b256c143
Farhan <fmshyder@gmail.com>
2019-04-20 22:41:32 -0400

    getLevel skeleton

--- front-end/src/Components/Map/mapFunctions.js
+++ front-end/src/Components/Map/mapFunctions.js
@@ -5,6 +5,18 @@ const CRIMSON_RED = "#d73027";          // lvl (3/6)
 const ORANGE = "#fc8d59";               // lvl (2/6)
 const NAVAJO_WHITE = "#fee08b";         // lvl (1/6)
 
-var test = 8;
+const AVERAGE_ALL = 110;    // FIXME: fetch this data from database
+const HIGHEST = 200;        // FIXME: fetch this data from database
 
-console.log(ORANGE);
+
+
+// pre: a number as a param
+// post: return an integer between [1,6]
+const getLevel = (amount) =>{
+
+
+    return 7;
+
+}
+
+console.log(getColor(000));
