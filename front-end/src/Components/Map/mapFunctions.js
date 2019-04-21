e2507bf406c4f8076941e5641a2eb402d7bafa7a
Farhan <fmshyder@gmail.com>
2019-04-20 22:42:45 -0400

    getColor func added

--- front-end/src/Components/Map/mapFunctions.js
+++ front-end/src/Components/Map/mapFunctions.js
@@ -8,7 +8,27 @@ const NAVAJO_WHITE = "#fee08b";         // lvl (1/6)
 const AVERAGE_ALL = 110;    // FIXME: fetch this data from database
 const HIGHEST = 200;        // FIXME: fetch this data from database
 
+// pre: a number as a param
+// post: return color code
+const getColor = (amount) =>{
+    const level = getLevel(amount);
+    switch(level){
+        case 6:
+            return EMERALD_GREEN;
+        case 5:
+            return PARAKEET_GREEN;
+        case 4:
+            return LIME_GREEN;
+        case 3:
+            return CRIMSON_RED;
+        case 2:
+            return ORANGE; 
+        default:
+            return NAVAJO_WHITE;
 
+    }
+
+}
 
 // pre: a number as a param
 // post: return an integer between [1,6]
