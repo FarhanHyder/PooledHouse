0c8e9d6ba59a84a8badd5ba89e804a0c4d13118b
Farhan <fmshyder@gmail.com>
2019-04-20 22:18:27 -0400

    6 different color code added

---
+++ front-end/src/Components/Map/mapFunctions.js
@@ -0,0 +1,10 @@
+const EMERALD_GREEN = "#1a9850";        // lvl (6/6)
+const PARAKEET_GREEN = "#91cf60";       // lvl (5/6)
+const LIME_GREEN = "#d9ef8b";           // lvl (4/6)
+const CRIMSON_RED = "#d73027";          // lvl (3/6)
+const ORANGE = "#fc8d59";               // lvl (2/6)
+const NAVAJO_WHITE = "#fee08b";         // lvl (1/6)
+
+var test = 8;
+
+console.log(ORANGE);
