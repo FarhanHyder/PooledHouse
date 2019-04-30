12590151e0122738a6b29ac02fab37f46a0aa1ca
Bashir Ahamed <ahamedb1991@gmail.com>
2019-04-30 18:15:25 -0400

    update to latest version

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,14 +15,19 @@ import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
 import ToggleButton from 'react-bootstrap/ToggleButton'
 import Container from 'react-bootstrap/Container'
-import Image from 'react-bootstrap/Image'
 
 //for maps
 import styled from 'styled-components';
 
 // View Component
 import ViewTipInfo from './Components/ViewTipInfo/ViewTipInfo';
+import PositionOption from './Components/ViewTipInfo/PositionOption';
 import './Components/ViewTipInfo/ViewTipInfo.css';
+import ViewTipsAverage from './Components/ProcessTips/ProcessTips';
+import ProcessOption from './Components/ProcessTips/ProcessOption';
+import './Components/ProcessTips/ProcessTips.css';
+import ViewUserTips from './Components/ViewTipInfo/UserTipInfo';
+import ViewTipInfoList from './Components/ViewTipInfo/ViewTipInfoList';
 
 //aws imports
 import Amplify, { API, graphqlOperation, Auth } from 'aws-amplify';
@@ -38,7 +43,6 @@ import * as subscriptions from './graphql/subscriptions'
 
 import logo from './images/logo.png'
 
-
 Amplify.configure(awsmobile);
 Amplify.configure(aws_config);
 
@@ -52,13 +56,20 @@ class App extends Component {
       showSignUp: false,
       showTipUpdate: false,
       curr_user_username: '',
-      showListView: true,
-      showMapView: false,
+      // userProfile : "",
+      showListView: false,
+      showMapView: true,
+      detailList: false,
+      showUserTips: false,
+      processFilter: "Business",
+      positionFilter: "All Position"
     }
 
     this.handleSignUp = this.handleSignUp.bind(this);
     this.handleTipUpdate = this.handleTipUpdate.bind(this);
     this.handleChangeView = this.handleChangeView.bind(this);
+    this.handleProcess = this.handleProcess.bind(this);
+    this.handlePosition = this.handlePosition.bind(this);
   }
 
   handleSignUp = () => {
@@ -70,8 +81,9 @@ class App extends Component {
 
   handleTipUpdate = () => {
     this.setState({
-      showHome: !this.state.showHome,
-      showTipUpdate: !this.state.showTipUpdate
+      //showHome: !this.state.showHome,
+      showTipUpdate: !this.state.showTipUpdate,
+      showListView: !this.state.showListView
     })
   }
 
@@ -82,6 +94,33 @@ class App extends Component {
     })
   }
   
+  handleProcess =(event) => {
+    this.setState({
+      processFilter : event.target.value
+    })
+  }
+
+  handlePosition = (event) => {
+    this.setState({
+      positionFilter : event.target.value
+    })
+  }
+
+  handleHomeView = () => {
+    this.setState({
+      showHome: true,
+      showSignUp: false,
+      showTipUpdate: false,
+      curr_user_username: '',
+      // userProfile : "",
+      showListView: false,
+      showMapView: true,
+      detailList: false,
+      showUserTips: false,
+      processFilter: "Business",
+      positionFilter: "All Position"
+    })
+  }
   //this grabs username attribute from current user.  
   //componentDidMount is executed after the webpage is rendered,
   //allowing for the page to be reloaded with data from API calls?
@@ -90,25 +129,28 @@ class App extends Component {
     let current_user = await Auth.currentAuthenticatedUser();
     let un = current_user.username;
     this.setState({
+      // userProfile: current_user, // TODO
       curr_user_username: un
     })
   }
 
   render() {
-
     const home = (
       <Navbar className="bg-olive justify-content-between">
 
-        {/* TODO: update the logo with Navbar.Brand */}
       <Navbar.Brand>
-        <img
+        <a href='#' onClick={this.handleHomeView}>
+        <img 
         src={ logo }
         width="200"
         height="64"
         className="d-inline-block align-top"
         alt="Pooled House logo"
-      />
+        />
+        </a>
+
       </Navbar.Brand>
+      
         <Form inline>
           <FormControl type="text" placeholder="ex: upper manhattan" className="mr-sm" />
           <Button type="submit" variant="outline-light"><span>{"\uD83D\uDD0D"}</span></Button>
@@ -116,37 +158,58 @@ class App extends Component {
 
         <ButtonToolbar>
           <ToggleButtonGroup type="radio" name="options" defaultValue={1} onChange={this.handleChangeView}>
-            <ToggleButton value={1} variant="warning">List View</ToggleButton>
-            <ToggleButton value={2} variant="warning">Map View</ToggleButton>
+            <ToggleButton value={1} variant="warning">Map View</ToggleButton>
+            <ToggleButton value={2} variant="warning">List View</ToggleButton>
           </ToggleButtonGroup>
         </ButtonToolbar>
         
         <ButtonToolbar>
-          <Button 
+          {/* <Button 
             className="text-color-white" 
-            onClick={this.handleTipUpdate}>Tip Update
+            onClick={this.handleTipUpdate}> Add New Tips
+          </Button> */}
+          <Button onClick={()=>{this.setState({detailList : false, showUserTips: true}); this.handleTipUpdate()}}>
+            My Tips
           </Button>
         </ButtonToolbar>
       </Navbar>
       );
 
-      const list_view = (
+      const viewData = (
+        //the connect component queries our database
         <Connect query={graphqlOperation(queries.listTipEntrys)}>
         {({ data: { listTipEntrys }, loading, error }) => {
             if (error) return (<h3>Error</h3>);
             if (loading || !listTipEntrys) return (<h3>Loading...</h3>);
-            return (<ViewTipInfo tipInfo={listTipEntrys.items} /> );
+            return (
+              <ViewTipInfoList 
+                user={this.state.curr_user_username} 
+                tip_info={listTipEntrys.items} 
+                showUserTips={this.state.showUserTips}/>
+            )
         }}
         </Connect>
       );
+
+      let mapData = (
+        <Connect query={graphqlOperation(queries.listTipEntrys)}>
+        {({ data: { listTipEntrys }, loading, error }) => {
+            if (error) return (<h3>Error</h3>);
+            if (loading || !listTipEntrys) return (<h3>Loading...</h3>);
+            return (
+              <div>
+                <Map tip_info={listTipEntrys ? listTipEntrys.items : []} />
+              </div>
+            )
+        }}
+        </Connect>
+      )
 
     return (
       <div className="App">
         <div id="home"> { home } </div>
         {this.state.showTipUpdate ? <TipInfoForm handler={this.handleTipUpdate}/> : null }
-        {/* the connect component queries our database and then passes the query
-          result to the ListView function */} 
-        {this.state.showListView ? <div> { list_view } </div> : <div> <Map /> </div>}
+        {this.state.showListView ? <div id="listView"> {viewData} </div> : <div> {mapData} </div>}
       </div>
     );
   }
