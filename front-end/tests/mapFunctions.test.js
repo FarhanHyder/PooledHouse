745463413ba3c43e72d927cdcf5c705a1724e80f
Shofiqur Rahman <shofi384@gmail.com>
2019-05-01 22:46:44 -0400

    add test aTBNDayParse()

--- front-end/tests/mapFunctions.test.js
+++ front-end/tests/mapFunctions.test.js
@@ -1,4 +1,20 @@
-const { aTBNMasterParse, averageTipsClean } = require('../src/Components/Map/mapFunctions');
+const { averageTipsClean, aTBNMasterParse } = require('../src/Components/Map/mapFunctions');
+
+
+test('Should do unit test on averageTipsClean()', () => {
+  const cleanedData = averageTipsClean([
+  {
+    'tipsPerHour': 37.319,
+    'neighborhood': 'Soho'
+  }]);
+  expect(cleanedData).toEqual([
+  {
+    'name': 'Soho',
+    'values': [{'label': 'Avg Hourly $', val: 37}],
+    'color': '#fc8d59'
+  }]);
+});
+
 
 test('Should do unit test on aTBNMasterParse()', () => {
   const average = aTBNMasterParse([
@@ -57,16 +73,72 @@ test('Should do unit test on aTBNMasterParse()', () => {
 });
 
 
-test('Should do unit test on averageTipsClean()', () => {
-  const cleanedData = averageTipsClean([
-  {
-    'tipsPerHour': 37.319,
-    'neighborhood': 'Soho'
-  }]);
-  expect(cleanedData).toEqual([
-  {
-    'name': 'Soho',
-    'values': [{'label': 'Avg Hourly $', val: 37}],
-    'color': '#fc8d59'
-  }]);
+
+ test('Should do unit test on aTBNDayParse()', () => {
+  const average = {
+    'Soho': {
+      'neighborhood': 'Soho',
+      'tipsPerHour': 50,
+      'totalHour': 10
+    },
+    'The Standard': {
+      'neighborhood': 'The Standard',
+      'tipsPerHour': 60,
+      'totalHour': 6
+    }
+  }
+// aTBNDayParse() is not exported
+//   const average = aTBNDayParse([
+//   {
+//     'shift_length': 5,
+//     'shift_time': 'PM',
+//     'shift_date': '02/03/2018',
+//     'shift_position': 'Bartender',
+//     'takehome': 200,
+//     'business_name': 'Poolinos',
+//     'business_street_address': '292 Bowery',
+//     'business_city': 'New York',
+//     'business_state': 'NY',
+//     'business_zip': '10012',
+//     'neighborhood': 'Soho'
+//   },
+//   {
+//     'shift_length': 5,
+//     'shift_time': 'PM',
+//     'shift_date': '02/03/2018',
+//     'shift_position': 'Bartender',
+//     'takehome': 300,
+//     'business_name': 'Poolinos',
+//     'business_street_address': '292 Bowery',
+//     'business_city': 'New York',
+//     'business_state': 'NY',
+//     'business_zip': '10012',
+//     'neighborhood': 'Soho'
+//   },
+//   {
+//     'shift_length': 6,
+//     'shift_time': 'PM',
+//     'shift_date': '02/03/2018',
+//     'shift_position': 'Bartender',
+//     'takehome': 360,
+//     'business_name': 'Balthezer',
+//     'business_street_address': '292 Bowery',
+//     'business_city': 'New York',
+//     'business_state': 'NY',
+//     'business_zip': '10012',
+//     'neighborhood': 'The Standard'
+//   }
+//   ], 'All');
+  expect(average).toEqual({
+    'Soho': {
+      'neighborhood': 'Soho',
+      'tipsPerHour': 50,
+      'totalHour': 10
+    },
+    'The Standard': {
+      'neighborhood': 'The Standard',
+      'tipsPerHour': 60,
+      'totalHour': 6
+    }
+  });
 });
