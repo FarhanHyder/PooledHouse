e2df6f8d1b8d097ab29b87785f222446f9201a97
Farhan <fmshyder@gmail.com>
2019-05-02 11:52:15 -0400

    skeleton UserTips

--- front-end/src/Components/UserTips/UserTips.js
+++ front-end/src/Components/UserTips/UserTips.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import {Col, Row, Container} from 'react-bootstrap';
 import {Table, Button, Card, Form} from 'react-bootstrap';
+
+
+class UserTips extends React.Component {
+
+}
+
+export default UserTips;
