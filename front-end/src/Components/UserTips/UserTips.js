3107369793276ec9126299add645f64e4451385a
Farhan <fmshyder@gmail.com>
2019-05-02 12:10:40 -0400

    updated logo

--- front-end/src/Components/UserTips/UserTips.js
+++ front-end/src/Components/UserTips/UserTips.js
@@ -4,6 +4,16 @@ import {Table, Button, Card, Form} from 'react-bootstrap';
 
 
 class UserTips extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            myTips: true,
+            addNewTips: false,
+        };
+
+        this.handleMyTips = this.handleMyTips.bind(this);
+        this.handleAddNewTips = this.handleAddNewTips.bind(this);
+    }
 
 }
 
