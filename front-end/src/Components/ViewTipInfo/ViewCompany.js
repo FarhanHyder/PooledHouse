560853eda2300055ae1c2b9246cb934a18bf9d1b
Bashir Ahamed <ahamedb1991@gmail.com>
2019-05-06 19:56:35 -0400

    calling tipsHistory and Description

--- front-end/src/Components/ViewTipInfo/ViewCompany.js
+++ front-end/src/Components/ViewTipInfo/ViewCompany.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import Description from './CompanyDescription';
 
+import TipsHistory from './TipsHistory';
+
+const process = require('../ProcessTips/ProcessTips');
+
 class ViewCompany extends Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
+            BusinessName: "",
             CompanyView: false,
             Description: true,
             TipsInfo: false,
-            TipsHistory: false
+            TipsHistory: false,
         }
         this.companyViewHandler = this.companyViewHandler.bind(this);
         this.descriptionViewHandler = this.descriptionViewHandler.bind(this);
+        this.tipsInfoViewHandler = this.tipsInfoViewHandler.bind(this);
+        this.tipsHistoryViewHandler = this.tipsHistoryViewHandler.bind(this);
     }
 
     companyViewHandler = () => {
@@ -25,19 +32,36 @@ class ViewCompany extends Component {
             TipsHistory: false
         });
     };
-
+    tipsInfoViewHandler = () => {
+        this.setState({
+            Description: false,
+            TipsInfo: true,
+            TipsHistory: false
+        });
+    };
+    tipsHistoryViewHandler = () => {
+        this.setState({
+            Description: false,
+            TipsInfo: false,
+            TipsHistory: true
+        });
+    };
 
     render(){
+        // const tipsInfo = this.props.tipsInfo.filter(tips => (tips.business_name.includes(this.state.BusinessName)));
+        // const processedTips = process.averageTipsByBusiness(tipsInfo);
+        // console.log(processedTips);
         const Company = (
             <div>
                 <div className="menu">
                     <button type="primary" onClick={this.companyViewHandler}>Company Name</button>
                     <button type="primary" onClick = {this.descriptionViewHandler}>Description</button>
-                    <button>Reported Tips</button>
-                    <button>Tips History</button>
+                    <button type="primary" onClick = {this.tipsInfoViewHandler}>Full Reported Tips</button>
+                    <button type="primary" onClick = {this.tipsHistoryViewHandler}>Tips History</button>
                 </div>
-                <div>
-                    {this.state.Description? <Description/> : null}
+                <div className="companyProfile">
+                    {this.state.Description? <Description business = {this.state.BusinessName}/> : null}
+                    {this.state.TipsHistory? <TipsHistory tipsInfo = {this.props.tipsInfo}/>: null}
                 </div>
             </div>
         );
