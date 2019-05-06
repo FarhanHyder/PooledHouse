f9b10ff8368253a44a44a6873487e28c9ae1d7d3
Bashir Ahamed <ahamedb1991@gmail.com>
2019-05-05 20:09:57 -0400

    initial component for compnay description

---
+++ front-end/src/Components/ViewTipInfo/CompanyDescription.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Description = (props) => {
+    const desc = (
+        <div>
+            <div>All Reported Locations</div>
+            <div>All Reported Position</div>
+            <div>Average tips for this Company</div>
+        </div>
+    );
+    return desc;
+};
+
+
+export default Description;
