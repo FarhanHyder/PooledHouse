3c2e9f9f4b8fcd90581b480ea419a6af49c9de26
Farhan <fmshyder@gmail.com>
2019-05-08 14:53:54 -0400

    view my tips bug updated

--- front-end/src/Components/ViewTipInfo/ViewUserTips.js
+++ front-end/src/Components/ViewTipInfo/ViewUserTips.js
@@ -4,51 +4,40 @@
  */
 
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Container } from 'react-bootstrap';
 
 const ViewUserTips = (props) => {
     let userTips =  props.tipInfo.filter(tips => tips.user === props.user);
 
     // let filterdUserTips = props.position !== "All Position"? userTips.filter(tips => tips.shift_position === props.position) : tipInfo;
     
-    let tipInfo = userTips.map( (tips, index) => {
-        return (
+    let tipInfo = (
+        <Container fluid>
             <div>
-                {/* <h2 className="busTitle">{tips.business_name}</h2>
-                <div className="addr">{tips.business_address}</div>
-                <div>Position: {tips.shift_position}</div>
-                <div>Tips : ${tips.takehome}</div>
-                <div>Hours Worked : {tips.shift_length}</div>
-                <div>Work Shift : {tips.shift_time}</div>
-                <div>Date : {tips.shift_date}</div> */}
 
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Business</th>
-                            <th>Neighborhood</th>
-                            <th>Shift</th>
-                            <th>Takehome</th>
-                            <th>Shift Length</th>
-                            <th>Shift Time</th>
-                            <th>Shift Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th>{tips.business_name}</th>
-                            <th>{tips.neighborhood}</th>
-                            <th>{tips.shift_position}</th>
-                            <th>{tips.takehome}</th>
-                            <th>{tips.shift_length}</th>
-                            <th>{tips.shift_time}</th>
-                            <th>{tips.shift_date}</th>
-                        </tr>
-                    </tbody>
-                </Table>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Business</th>
+                        <th>Neighborhood</th>
+                        <th>Hourly Avg $</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { Object.keys(userTips).map(key => {
+                        return (
+                            <tr>
+                                <td>{ userTips[key].business }</td>
+                                <td>{ userTips[key].neighborhood }</td>
+                                <td>{ Math.floor(userTips[key].tipsPerHour) }</td>
+                            </tr>
+                        )
+                    }) }
+                </tbody>
+            </Table>
             </div>
-        );        
-    });
+        </Container>
+    );
 
     return tipInfo;
 }
