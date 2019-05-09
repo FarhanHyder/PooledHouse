60aac59072c3610529ffc0d36e4234b0b134950a
Bashir Ahamed <ahamedb1991@gmail.com>
2019-05-09 07:09:36 -0400

    complete description and package.json update for react-google-char

--- front-end/src/Components/ViewTipInfo/CompanyDescription.js
+++ front-end/src/Components/ViewTipInfo/CompanyDescription.js
@@ -6,14 +6,14 @@ const Description = (props) => {
         return (
             <div className="row border border-primary rounded-lg">
                 <div className="col-4">{loc}</div>
-                <div className="col-2">${Number.parseFloat(props.locations[loc].tipsPerHour).toFixed(2)} / Hour</div>  
+                <div className="col-2">${Number.parseFloat(props.locations[loc].tipsPerHour.toFixed(2))} / Hour</div>  
             </div>
         )
     });
     
     const desc = (
-        <div className="container">
-            <div className="row border border-success rounded-lg">
+        <div className="container card">
+            <div className=" card-title row border border-success rounded-lg">
                 <div className="col-4">Company Locations</div>
                 <div className="col-2">Average Tips</div>
             </div>
