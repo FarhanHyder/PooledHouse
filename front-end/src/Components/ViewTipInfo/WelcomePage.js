bf10f5a222adce328e59e0b19d8bab6a628cf912
Bashir Ahamed <ahamedb1991@gmail.com>
2019-05-12 17:06:28 -0400

    welcome component for map and list view

--- front-end/src/Components/ViewTipInfo/WelcomePage.js
+++ front-end/src/Components/ViewTipInfo/WelcomePage.js
@@ -25,29 +25,7 @@ class WelcomePage extends React.Component {
       const { index, direction } = this.state;
   
       return (
-          <div>
-            {/* <>
-              <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Pool</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">View Map</Nav.Link>
-                </Nav>
-                <Nav>
-                <Form inline onSubmit={(event)=>{ event.preventDefault(); }}>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange = {(event)=> {this.props.searchHandler(this.props.tipsInfo, event.target.value)}} />
-                <Button variant="outline-info" >Search</Button>
-                </Form>
-                </Nav>
-                <DropdownButton id="dropdown-basic-button" title="Me">
-                    <Dropdown.Item href="#/action-1">My Tips</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                </DropdownButton>;
-                
-            </Navbar>
-            </>; */}
-          
+          <div className = "container, fluid">     
           <Card bg="info" text="white" style={{  }}>
             <Card.Header>Welcome to Pooled House</Card.Header>
             <Card.Body>
@@ -55,6 +33,18 @@ class WelcomePage extends React.Component {
               <Card.Text>
                 You can find all tips information about any company.
                 Enter the name of the company in the search box to see the reported tips.
+              </Card.Text>
+              <Form></Form>
+            </Card.Body>
+          </Card>
+          <br></br>
+          <Card bg="info" text="white" style={{  }}>
+            <Card.Header>See Companies in Map</Card.Header>
+            <Card.Body>
+              <Card.Title>Click on See Map to View Tips for different Locations</Card.Title>
+              <Card.Text>
+                Filter by Position, Day, Shift to see Locations.
+                The Map shows All the Average Tips By Area / Neighborhood. Click on the 
               </Card.Text>
               <Form></Form>
             </Card.Body>
