d6a2b27ff883a845d842388ead2a853b412d414d
Bashir Ahamed <ahamedb1991@gmail.com>
2019-05-13 14:57:10 -0400

    update to reported tips

--- front-end/src/Components/ViewTipInfo/ViewReportedTips.js
+++ front-end/src/Components/ViewTipInfo/ViewReportedTips.js
@@ -15,33 +15,47 @@ class ViewReportedTips extends React.Component {
     render() {
         const view = (
             <div className="container">
-                <div className="row">
-                    <div className="col-2 btn-group-vertical position-left">
-                        <button type="button" className="btn btn-dark active" onClick ={()=>{this.setState({chartOption: "position"})}} >Tips By Position</button>
+                <div className="card">
+                    <br></br>
+                    <div className="row btn-group position-left">
+                        <button type="button" className="btn btn-dark" onClick ={()=>{this.setState({chartOption: "position"})}} >Tips By Position</button>
                         <button type="button" className="btn btn-dark" onClick ={()=>{this.setState({chartOption: "day"})}} >Tips By Day</button>
                         <button type="button" className="btn btn-dark" onClick ={()=>{this.setState({chartOption: "zip"})}} >Tips By Zip Code</button>
                         <button type="button" className="btn btn-dark" onClick ={()=>{this.setState({chartOption: "neighborhood"})}} >Tips By neighborhood</button>
                         <button type="button" className="btn btn-dark" onClick ={()=>{this.setState({chartOption: "shift"})}} >Tips By Shift</button>
                     </div>
-                    <div className="col-10 card">
+                    <br></br>
+                </div>
+                <div className="row">
+                    <div className="col-4">
                         {this.state.chartOption === "position"?
                             <Table striped bordered hover responsive="sm" variant="dark">
                                 <thead className="bg-secondary">
                                 <tr>
-                                    <th>Bartender</th>
-                                    <th>Server</th>
-                                    <th>Barback</th>
-                                    <th>Busser</th>
-                                    <th>Other</th>
+                                    <th>Position</th>
+                                    <th>Hourly Tips</th>
                                 </tr>
                                 </thead>
                                 <tbody>
                                 <tr>
-                                    <td>{Number.parseFloat(this.props.avgByPosition.Bartender.tipsPerHour.toFixed(2))}</td>
-                                    <td>{Number.parseFloat(this.props.avgByPosition.Server.tipsPerHour.toFixed(2))}</td>
-                                    <td>{Number.parseFloat(this.props.avgByPosition.Barback.tipsPerHour.toFixed(2))}</td>
-                                    <td>{Number.parseFloat(this.props.avgByPosition.Busser.tipsPerHour.toFixed(2))}</td>
-                                    <td>{Number.parseFloat(this.props.avgByPosition.Other.tipsPerHour.toFixed(2))}</td>
+                                    <td>Bartender</td>
+                                    <td>${Number.parseFloat(this.props.avgByPosition.Bartender.tipsPerHour.toFixed(2))}</td>
+                                </tr>
+                                <tr>
+                                    <td>Server</td>
+                                    <td>${Number.parseFloat(this.props.avgByPosition.Server.tipsPerHour.toFixed(2))}</td>
+                                </tr>
+                                <tr>
+                                    <td>Barback</td>
+                                    <td>${Number.parseFloat(this.props.avgByPosition.Barback.tipsPerHour.toFixed(2))}</td>
+                                </tr>
+                                <tr>
+                                    <td>Busser</td>
+                                    <td>${Number.parseFloat(this.props.avgByPosition.Busser.tipsPerHour.toFixed(2))}</td>
+                                </tr>
+                                <tr>
+                                    <td>Other</td>
+                                    <td>${Number.parseFloat(this.props.avgByPosition.Other.tipsPerHour.toFixed(2))}</td>
                                 </tr>
                                 </tbody>
                             </Table>
@@ -51,42 +65,63 @@ class ViewReportedTips extends React.Component {
                             <Table striped bordered hover responsive="sm" variant="dark">
                                 <thead className="bg-secondary">
                                 <tr>
-                                    <th>Saturday</th>
-                                    <th>Sunday</th>
-                                    <th>Monday</th>
-                                    <th>Tuesday</th>
-                                    <th>Wednesday</th>
-                                    <th>Thursday</th>
-                                    <th>Friday</th>
+                                    <th>Day</th>
+                                    <th>Hourly Tips</th>
                                 </tr>
                                 </thead>
                                 <tbody>
                                 <tr>
-                                    <td>{Number.parseFloat(this.props.avgByDay.Saturday.tipsPerHour.toFixed(2))}</td>
-                                    <td>{Number.parseFloat(this.props.avgByDay.Sunday.tipsPerHour.toFixed(2))}</td>
-                                    <td>{Number.parseFloat(this.props.avgByDay.Monday.tipsPerHour.toFixed(2))}</td>
-                                    <td>{Number.parseFloat(this.props.avgByDay.Tuesday.tipsPerHour.toFixed(2))}</td>
-                                    <td>{Number.parseFloat(this.props.avgByDay.Wednesday.tipsPerHour.toFixed(2))}</td>
-                                    <td>{Number.parseFloat(this.props.avgByDay.Thursday.tipsPerHour.toFixed(2))}</td>
-                                    <td>{Number.parseFloat(this.props.avgByDay.Friday.tipsPerHour.toFixed(2))}</td>
+                                    <td>Saturday</td>
+                                    <td>${Number.parseFloat(this.props.avgByDay.Saturday.tipsPerHour.toFixed(2))}</td>
+                                </tr>
+                                <tr>
+                                    <td>Sunday</td>
+                                    <td>${Number.parseFloat(this.props.avgByDay.Sunday.tipsPerHour.toFixed(2))}</td>
+                                </tr>
+                                <tr>
+                                    <td>Monday</td>
+                                    <td>${Number.parseFloat(this.props.avgByDay.Monday.tipsPerHour.toFixed(2))}</td>
+                                </tr>
+                                <tr>
+                                    <td>Tuesday</td>
+                                    <td>${Number.parseFloat(this.props.avgByDay.Tuesday.tipsPerHour.toFixed(2))}</td>
+                                </tr>
+                                <tr>
+                                    <td>Wednesday</td>
+                                    <td>${Number.parseFloat(this.props.avgByDay.Wednesday.tipsPerHour.toFixed(2))}</td>
+                                </tr>
+                                <tr>
+                                    <td>Thursday</td>
+                                    <td>${Number.parseFloat(this.props.avgByDay.Thursday.tipsPerHour.toFixed(2))}</td>
+                                </tr>
+                                <tr>
+                                    <td>Friday</td>
+                                    <td>${Number.parseFloat(this.props.avgByDay.Friday.tipsPerHour.toFixed(2))}</td>
                                 </tr>
                                 </tbody>
                             </Table>
                         : null}
 
-                        <div>Average Tips in this Zip Code: $ / Hours</div>
+                        {/* <div>Average Tips in this Zip Code: $ / Hours</div>
                         <div>Average in this neighborhood</div>
-                        <div>Average Tips for different Position</div>
+                        <div>Average Tips for different Position</div> */}
                     </div>
-                
-                </div>
-                <div className="">
+                    <div className="col-8">
                     <ViewBarChart
                         chartOption = {this.state.chartOption}
                         avgByDay = {this.props.avgByDay}
                         avgByPosition = {this.props.avgByPosition}
                     />
+                    </div>
+                
                 </div>
+                {/* <div className="">
+                    <ViewBarChart
+                        chartOption = {this.state.chartOption}
+                        avgByDay = {this.props.avgByDay}
+                        avgByPosition = {this.props.avgByPosition}
+                    />
+                </div> */}
                 
             </div>
         );
