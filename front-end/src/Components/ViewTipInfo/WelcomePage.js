69d075eab97384391083f10199609c3791dc7db9
IanMFraser <hunter_gracchus@hotmail.com>
2019-05-13 13:52:54 -0400

    welcome page about

--- front-end/src/Components/ViewTipInfo/WelcomePage.js
+++ front-end/src/Components/ViewTipInfo/WelcomePage.js
@@ -1,3 +1,7 @@
+/** 
+ * @class WelcomePage
+ * @extends Component
+ */
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import {Button, Dropdown, DropdownButton, Navbar, Nav, Form, FormControl, Card} from 'react-bootstrap'
@@ -30,10 +34,20 @@ class WelcomePage extends React.Component {
           <Card bg="info" text="white" style={{  }}>
             <Card.Header>Welcome to Pooled House</Card.Header>
             <Card.Body>
-              <Card.Title>Search company</Card.Title>
+              {/* <Card.Title>Search company</Card.Title>
               <Card.Text>
                 You can find all tips information about any company.
                 Enter the name of the company in the search box to see the reported tips.
+              </Card.Text> */}
+              <Card.Title>A social income tracker for restaurant industry professionals
+</Card.Title>
+              <Card.Text>
+                Pooled House allows restaurant industry professionals to share information about their
+                tip earnings with other professionals. The app allows users to keep track of their income,
+                and share with it with others to help workers gain better insight to where the most money
+                is made across their city.
+
+                Search by location or business to see where the most money is made. 
               </Card.Text>
               <Form></Form>
             </Card.Body>
